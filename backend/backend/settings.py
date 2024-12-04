f50d3f903385455fc350ebfcf47ae444171667bc
aketterjr <aketter@smu.edu>
2024-12-03 19:45:32 -0600

    reconfiguing database and adding dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,12 +97,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_data',
+        'USER' : 'Andre',
+        'PASSWORD' : '5CVW931h$',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
